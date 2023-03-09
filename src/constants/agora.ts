9c48c2cbb579e4d348a7ce89d38c55f21e5893ad
unique.mo <myx3901051@qq.com>
2023-03-09 18:22:01 +0800

    feat: update token

--- src/constants/agora.ts
+++ src/constants/agora.ts
@@ -1,3 +1,3 @@
 export const APP_ID = '653e29d5bb314ed194619ee59341497a';
-export const TOKEN = '007eJxTYJjSJ31UwMbFsmJy1LtNh++/TpizzifveaRj56VF1xfN5Q1UYDAzNU41skwxTUoyNjRJTTG0NDEztExNNbU0NjE0sTRPNPNiTWkIZGQozL3CysgAgSA+C0NJanEJAwMAagofPw==';
+export const TOKEN = '007eJxTYOAQXforsjWeTfDGC2OP7S9X/FlkvG3+vFR3u3ea+fc+nW9QYDAzNU41skwxTUoyNjRJTTG0NDEztExNNbU0NjE0sTRP5FzGltIQyMggzp/PwAiFID4LQ0lqcQkDAwBYlx6A';
 export const CHANNEL = 'test';
