364649f460fb6d7b966389da1ca2b40ea9c5fea5
unique.mo <myx3901051@qq.com>
2023-12-10 14:58:02 +0800

    update agora token

--- src/constants/agora.ts
+++ src/constants/agora.ts
@@ -1,3 +1,3 @@
 export const APP_ID = '79be7db26a6f4950ad91386035ea3b42';
-export const TOKEN = '007eJxTYOhsVZj13/7LqcUf7iyUkfi3+IvcpWjBy5NbngbOvVh64chUBQZzy6RU85QkI7NEszQTS1ODxBRLQ2MLMwNj09RE4yQTo3sHilIbAhkZWB4YMDEyQCCYz1CSWlzCwAAAbqgi8Q==';
+export const TOKEN = '007eJxTYDjqH8Lr/6On+0WNhHXe/qJPxVfqN9Wed317+fFyzf2Hoo4rMJhbJqWapyQZmSWapZlYmhokplgaGluYGRibpiYaJ5kYySeWpjYEMjLUzbjAwsgAgSA+C0NJanEJAwMAAhghvg==';
 export const CHANNEL = 'test';
